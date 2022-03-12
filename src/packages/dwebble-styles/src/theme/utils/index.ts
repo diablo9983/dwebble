d7f0cac4e9a31d19785c25d7071def2196d772ad
diablo9983 <diablo9983@gmail.com>
2022-03-12 20:12:11 +0100

    fix(styles): Export DwebbleGradient type

--- src/packages/dwebble-styles/src/theme/utils/index.ts
+++ src/packages/dwebble-styles/src/theme/utils/index.ts
@@ -1,2 +1,3 @@
 export { extractMargins } from "./extract-margins/extract-margins";
 export { getSharedColorScheme } from "./get-shared-color-scheme";
+export type { DwebbleGradient } from "./get-shared-color-scheme";
